bdc0c3451b068463087e1bdb1b0184ca04b2e819
José Gramajo <gra22907@uvg.edu.gt>
2024-10-01 21:54:28 -0600

    fix: Corrección del cálculo de la especularidad para las luces direccionales

--- lights.py
+++ lights.py
@@ -1,6 +1,6 @@
 from math import pi, cos
 from gl import *
-from mathlib import *
+from MathLib import *
 
 
 class Light(object):
@@ -19,39 +19,6 @@ class Light(object):
 class AmbientLight(Light):
     def __init__(self, color=[1, 1, 1], intensity=1.0):
         super().__init__(color, intensity, "Ambient")
-
-
-class DirectionalLight(Light):
-    def __init__(self, color=[1, 1, 1], intensity=1.0, direction=[0, -1, 0]):
-        super().__init__(color, intensity, "Directional")
-        self.direction = normalize_vector(direction)
-
-    def GetLightColor(self, intercept=None):
-        lightColor = super().GetLightColor()
-        if intercept:
-            dir = [(i * -1) for i in self.direction]
-            intensity = dot(intercept.normal, dir)
-            intensity = max(0, min(1, intensity))
-            intensity *= (1 - intercept.obj.material.Ks)
-            lightColor = [(i * intensity) for i in lightColor]
-        return lightColor
-
-    def GetSpecularColor(self, intercept, viewPos):
-        specColor = self.color
-
-        if intercept:
-            dir = [(i * -1) for i in self.direction]
-            reflect = calc_reflection(intercept.normal, dir)
-
-            viewDir = sub_elements(viewPos, intercept.point)
-            viewDir = normalize_vector(viewDir)
-
-            specularity = max(0, dot(viewDir, reflect) ** intercept.obj.material.spec)
-            specularity *= intercept.obj.material.Ks
-            specularity *= self.intensity
-            specColor = [(i * specularity) for i in specColor]
-
-        return specColor
 
 
 class PointLight(Light):
@@ -100,6 +67,41 @@ class PointLight(Light):
 
             if R != 0:
                 specularity /= R ** 2
+
+            specColor = [i * specularity for i in specColor]
+
+        return specColor
+
+
+class DirectionalLight(Light):
+    def __init__(self, color=[1, 1, 1], intensity=1.0, direction=[0, -1, 0]):
+        super().__init__(color, intensity, "Directional")
+        self.direction = normalize_vector(direction)
+
+    def GetLightColor(self, intercept=None):
+        lightColor = super().GetLightColor()
+        if intercept:
+            dir = [-i for i in self.direction]
+            intensity = dot(intercept.normal, dir)
+            intensity = max(0, min(1, intensity))
+            intensity *= (1 - intercept.obj.material.Ks)
+            lightColor = [(i * intensity) for i in lightColor]
+        return lightColor
+
+    def GetSpecularColor(self, intercept, viewPos):
+        specColor = self.color
+
+        if intercept:
+            dir = [-i for i in self.direction]  # Dirección ya normalizada
+            reflect = calc_reflection(intercept.normal, dir)
+
+            viewDir = sub_elements(viewPos, intercept.point)
+            viewDir = normalize_vector(viewDir)
+
+            # Evitar el uso de números complejos en el cálculo de especularidad
+            specularity = max(0, abs(dot(viewDir, reflect)) ** intercept.obj.material.spec)
+            specularity *= intercept.obj.material.Ks
+            specularity *= self.intensity
 
             specColor = [i * specularity for i in specColor]
 
